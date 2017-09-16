8fd3244b3fd7c39f6672376868379eb3865ada85
ronishz <ronishzadode@gmail.com>
2017-09-17 03:01:18 +0530

    jhol

--- groups.py
+++ groups.py
@@ -94,18 +94,18 @@ check=[]
 for i in range(len(g)):
 	check.append(g[i]["group_id"])
 check.sort()
-'''	
-#print(check)
+	'''
+print(check)
 #print(data[grps[2][13]]["latitude"])	
 #print(len(grps[16]))
-
+'''
 for i in range(0,len(g)):
 	data[i].update({'group_id':g[i]['group_id']})
 
 
 f1=open("group_location_data.json","w")
 json.dump(data,f1)
-
+'''
 #print(angle())
 
 
