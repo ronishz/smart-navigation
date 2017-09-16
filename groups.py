e9d7bc64c62f671d3ea9f7a217261f2c61e7e250
ronishz <ronishzadode@gmail.com>
2017-09-16 22:45:08 +0530

    new id added

--- groups.py
+++ groups.py
@@ -1,6 +1,8 @@
 import json
 f=open("final_location_data.json","r")
 data=json.load(f)
+data=data["dB"]
+#print(data[0])
 
 def findGroupId(dist):
 	if(dist<=34):
@@ -28,15 +30,27 @@ for i in range(0,len(data)):
 	temp.update({'user_id':i+1})
 	g.append(temp)
 #print(g)
+'''
 temp_g=[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+'''
+grps=[[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
 for i in range(len(g)):
 	x=g[i]["group_id"]
 	x-=1
-	temp_g[x]+=1
-print(temp_g)
-
-
-
-
-	
+	grps[x].append(g[i]["user_id"])
+print(grps)
+'''
+temp=[]
+for i in range(0,len(grps)):
+		#temp=grps[i]
+		if(len(grps[i]))>1:
+			for j in range(len(grps[i])):
+				temp_lat=0
+				temp_lng=0
+				temp_lat=data[grps[i][j]]["latitude"]
+				temp_lng=data[grps[i][j]]["longitude"]
+				print(temp_lat)
+				print(temp_lng)
+'''
+print(data[grps[17][0]]["latitude"])	
 	
