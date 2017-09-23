3e9bea378e5508b35898cea88aa3c47cf2c2e5b3
ronishz <ronishzadode@gmail.com>
2017-09-24 00:34:54 +0530

    Add new entry

--- mail.py
+++ mail.py
@@ -72,6 +72,11 @@ while True:
 	id_cnt=int(id_cnt)
 	sheet_cnt=int(sheet_cnt)
 	mail_list = []
+	os.system("git add *")
+	os.system("git commit -m \"Add new entry\"")
+	os.system("git push")
+	
+	
 	for i in range(sheet_cnt+1,len(v)):
 		temp = co.OrderedDict()
 		id_cnt+=1
